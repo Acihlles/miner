f23570ccdf9fb4b87c5f511f1e3d72e116866922
Acihlles <142124579+Acihlles@users.noreply.github.com>
2024-10-17 20:53:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,156 @@
+testcatch = False
+import time
+import sys
+import random
+
+# module required
+try:
+    import colorama
+except Exception:
+    print("You are missing colorama")
+    testcatch = True
+
+import os
+
+# Ran after exception
+if testcatch:
+    while True:
+        fix = input("Install Colorama? (y/n): ")
+        if fix == "y" or fix == "n":
+            break
+        else:
+            print("Please enter a valid choice!")
+    # if yes then
+    if fix == "y":
+        try:
+            os.system("py -m pip install colorama")
+        except Exception:
+            print("There was an error whilst trying to download the module!")
+            print("Please try to manually download or contact a developer for support.")
+            exit()
+        print("Module successfully installed!")
+        print("Restart program.")
+        input("")
+        exit()
+    # if no then
+    if fix == "n":
+        print("Alright!")
+        print("Press 'Enter' to continue.")
+        input("")
+        exit()
+colorama.init(autoreset=False)
+
+
+def clr():
+    _ = os.system("cls")
+    return
+
+
+def print00025(text):
+    for c in text:
+        sys.stdout.write(colorama.Fore.CYAN + c)
+        sys.stdout.flush()
+        time.sleep(0.0025)
+    sys.stdout.write("\n")
+
+
+def print0040(text):
+    for c in text:
+        sys.stdout.write(colorama.Fore.CYAN + c)
+        sys.stdout.flush()
+        time.sleep(0.040)
+    sys.stdout.write("\n")
+
+
+def miner():
+    delay = 0.25
+    delay2 = 3
+    print00025("""
+ _______  _______  _______    _     _  _______  ___      ___      _______  _______    __   __  ___   __    _  _______  ______   
+|  _    ||       ||       |  | | _ | ||   _   ||   |    |   |    |       ||       |  |  |_|  ||   | |  |  | ||       ||    _ |  
+| |_|   ||_     _||       |  | || || ||  |_|  ||   |    |   |    |    ___||_     _|  |       ||   | |   |_| ||    ___||   | ||  
+|       |  |   |  |       |  |       ||       ||   |    |   |    |   |___   |   |    |       ||   | |       ||   |___ |   |_||_ 
+|  _   |   |   |  |      _|  |       ||       ||   |___ |   |___ |    ___|  |   |    |       ||   | |  _    ||    ___||    __  |
+| |_|   |  |   |  |     |_   |   _   ||   _   ||       ||       ||   |___   |   |    | ||_|| ||   | | | |   ||   |___ |   |  | |
+|_______|  |___|  |_______|  |__| |__||__| |__||_______||_______||_______|  |___|    |_|   |_||___| |_|  |__||_______||___|  |_|
+
+                                    # @r4ckiez """)
+    print0040("Testing proxies...")
+    time.sleep(delay)
+    r1 = random.randint(1, 3)
+    print0040("Starting " + str(r1) + " Server(s)")
+    time.sleep(delay)
+    print0040("Server(s) have started")
+    time.sleep(delay)
+    print0040("Pushing update to Server(s)")
+    time.sleep(delay)
+    print0040("Starting program...")
+    time.sleep(3)
+    choice = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+              "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+              "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    while True:
+        clr()
+        print(colorama.Fore.CYAN + """
+ _______ _______ _______   _     _ _______ ___     ___     _______ _______   __   __ ___ __    _ _______ ______   
+|  _    |       |       | | | _ | |   _   |   |   |   |   |       |       | |  |_|  |   |  |  | |       |    _ |  
+| |_|   |_     _|       | | || || |  |_|  |   |   |   |   |    ___|_     _| |       |   |   |_| |    ___|   | ||  
+|       | |   | |       | |       |       |   |   |   |   |   |___  |   |   |       |   |       |   |___|   |_||_ 
+|  _   |  |   | |      _| |       |       |   |___|   |___|    ___| |   |   |       |   |  _    |    ___|    __  |
+| |_|   | |   | |     |_  |   _   |   _   |       |       |   |___  |   |   | ||_|| |   | | |   |   |___|   |  | |
+|_______| |___| |_______| |__| |__|__| |__|_______|_______|_______| |___|   |_|   |_|___|_|  |__|_______|___|  |_|
+
+                                            # @r4ckiez""")
+        for u in range(500):
+            rc = random.choices(choice, k=64)
+            print(colorama.Fore.CYAN + "Generated Code - " + "".join(rc) + " - 0.00$")
+            time.sleep(0.075)
+        clr()
+        r1 = random.randint(1, 200)
+        rc = random.choices(choice, k=64)
+
+        print(colorama.Fore.GREEN + """
+ _______  _______  _______    _     _  _______  ___      ___      _______  _______    __   __  ___   __    _  _______  ______   
+|  _    ||       ||       |  | | _ | ||   _   ||   |    |   |    |       ||       |  |  |_|  ||   | |  |  | ||       ||    _ |  
+| |_|   ||_     _||       |  | || || ||  |_|  ||   |    |   |    |    ___||_     _|  |       ||   | |   |_| ||    ___||   | ||  
+|       |  |   |  |       |  |       ||       ||   |    |   |    |   |___   |   |    |       ||   | |       ||   |___ |   |_||_ 
+|  _   |   |   |  |      _|  |       ||       ||   |___ |   |___ |    ___|  |   |    |       ||   | |  _    ||    ___||    __  |
+| |_|   |  |   |  |     |_   |   _   ||   _   ||       ||       ||   |___   |   |    | ||_|| ||   | | | |   ||   |___ |   |  | |
+|_______|  |___|  |_______|  |__| |__||__| |__||_______||_______||_______|  |___|    |_|   |_||___| |_|  |__||_______||___|  |_|
+
+                                    # @r4ckiez""")
+        print(colorama.Fore.GREEN + "Generated Code - " + "".join(rc) + " - " + str(r1) + "$")
+        time.sleep(1)
+        print(colorama.Fore.GREEN + "We are sending money to your address!")
+        time.sleep(1)
+        print(colorama.Fore.GREEN + "@r4ckiez")
+        time.sleep(5)
+        return
+
+
+while True:
+    clr()
+    print(colorama.Fore.CYAN + """
+--------------------------------------
+       BTC WALLET MINER BY @r4ckiez
+--------------------------------------
+    [1] Credits
+    
+    [2] Change Wallet Address
+    
+    [3] Crypto Wallet Miner
+--------------------------------------""")
+    print(colorama.Fore.GREEN + "")
+    main = input("> ")
+    if main == "3":
+        miner()
+    if main == "1":
+        print("Made by @r4ckiez")
+        print("Do not share with anyone")
+        print("")
+        print(colorama.Fore.GREEN + "Original program by:@r4ckiez")
+        print(colorama.Fore.RED + "instagram: @r4ckiez ")
+        print("")
+        input("")
+    if main == "2":
+        main = input("Please enter your wallet address: ")
